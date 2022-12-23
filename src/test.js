a1ea908a99396079cbe7bbb45c330a82896056e2
kochizufan <kochizufan@gmail.com>
2022-12-23 23:05:48 +0900

    It works

--- src/test.js
+++ src/test.js
@@ -1,37 +1,6 @@
-/*const path = require( "node:path");
-const fs = require("fs");
-
-function fileUrl(filePath, options = {}) {
-  if (typeof filePath !== 'string') {
-    throw new TypeError(`Expected a string, got ${typeof filePath}`);
-  }
-
-  const {resolve = true} = options;
-
-  let pathName = filePath;
-  if (resolve) {
-    pathName = path.resolve(filePath);
-  }
-
-  pathName = pathName.replace(/\\/g, '/');
-
-  // Windows drive letter must be prefixed with a slash.
-  if (pathName[0] !== '/') {
-    pathName = `/${pathName}`;
-  }
-
-  // Escape required characters for path components.
-  // See: https://tools.ietf.org/html/rfc3986#section-3.3
-  return encodeURI(`file://${pathName}`).replace(/[?#]/g, encodeURIComponent);
-}*/
+const { serialize, deserialize } = require("./flatgeobuf-geojson.min.js");
 
 const main = async () => {
-  //console.log(__dirname);
-  //const list = fs.readdir(path.resolve(__dirname, "./"));
-  //console.log(list);
-  //const { geojson } = await import(fileUrl(path.resolve(__dirname, "./node_modules/flatgeobuf/lib/mjs/flatgeobuf.js")));
-  console.log("HOge");
-  const { geojson } = await import("../node_modules/flatgeobuf/lib/mjs/flatgeobuf.js");
   const expected = {
     type: 'FeatureCollection',
     features: [{
@@ -43,8 +12,11 @@ const main = async () => {
     }]
   };
 
-  //const flatgeobuf = geojson.serialize(expected);
-  //console.log(`Serialized input GeoJson into FlatGeobuf (${flatgeobuf.length} bytes)`);
+  const flatgeobuf = serialize(expected);
+  console.log(`Serialized input GeoJson into FlatGeobuf (${flatgeobuf.length} bytes)`);
+  const actual = deserialize(flatgeobuf);
+  console.log(`Deserialized FlatGeobuf into GeoJson`);
+  console.log(actual);
   console.log("It works!");
 };
 
